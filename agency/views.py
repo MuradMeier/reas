8feed7831a0db707e14534ac7f0cc066c8acd5bf
Murad Akhmedkhanov <muradahmedhanov8779@gmail.com>
2026-03-18 17:29:26 +0300

    feat(backend): add search-object endpoint for auto-filling object data2

--- agency/views.py
+++ agency/views.py
@@ -7,7 +7,7 @@ from django.http.response import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, status, filters
-from rest_framework.decorators import action
+from rest_framework.decorators import action, api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.pagination import PageNumberPagination
